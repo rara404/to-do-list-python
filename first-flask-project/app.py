71052eaeb671af623bf025b9672fcc0630877747
Raj Rana <raj.rana@fiserv.com>
2023-12-04 09:55:26 -0500

    fixed bugs

--- first-flask-project/app.py
+++ first-flask-project/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, jsonify
+import pandas as pd
+
 
 app = Flask(__name__)
 
@@ -9,21 +10,37 @@ def helloworld():
     return render_template("index.html")
 
 
-# @app.route("/upload", methods=["POST"])
-# def upload():
-#     # Check if the request contains a file
-#     if "file" in request.files:
-#         # Get the file from the request
-#         file = request.files["file"]
-#         # Read the file as a pandas dataframe
-#         df = pd.read_excel(file)
-#         # Convert the dataframe to a list of lists
-#         data = df.values.tolist()
-#         # Render the HTML template for displaying the data
-#         return render_template("data_view.html", data=data)
-#     else:
-#         # Return an error message if no file was uploaded
-#         return "No file was uploaded."
+@app.route("/upload", methods=["GET"])
+def upload():
+    try:
+        file_path = 'todo_tasks_data.xlsx'
+        df = pd.read_excel(file_path)
+        raw_data = df.values.tolist()
+    
+        to_do_list = []
+    
 
+        
+        for i in raw_data:
+            task_split = i[0].split(", ")
+    
+            task_map = {
+                "Task Name": task_split[0],
+                "Date Created": task_split[1],
+                "Date Completed": task_split[2],
+                "Completion Status": task_split[3],
+                "Priority": task_split[4],
+                "Time to Complete": task_split[5]
+            }
+    
+            to_do_list.append(task_map)
+        
+
+        return jsonify(to_do_list)
+    
+    except Exception as e:
+        error_message = f"Error: {str(e)}"
+        return render_template('index.html', error=error_message)
+    
 
 app.run(debug=True)
